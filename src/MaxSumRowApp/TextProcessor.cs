6f0702707a5acdefd931af3b8ecbb0d118abebe5
EasyQuery team lead <easyquery@korzh.com>
2022-10-30 00:47:38 +0300

    initial commit

---
+++ src/MaxSumRowApp/TextProcessor.cs
@@ -0,0 +1,42 @@
+﻿namespace MaxSumRowApp;
+
+public class TextProcessor
+{
+    private TextReader _reader;
+
+    public TextProcessor(TextReader reader)
+    {
+        _reader = reader;
+    }
+
+    public SortedSet<int> DefectiveRows { get; } = new SortedSet<int>();
+
+    public async Task<int> GetMaxSumRowNum()
+    {
+        string? row;
+        float maxSum = float.MinValue;
+        int maxSumRow = -1;
+        int rowNum = 0;
+        while ((row = await _reader.ReadLineAsync()) != null) {
+            float sum = 0;
+            var tokens = row.Split(',');
+            foreach (var token in tokens) {
+                if (float.TryParse(token.Trim(), out var fnum)) {
+                    sum += fnum;
+                }
+                else {
+                    DefectiveRows.Add(rowNum);
+                }
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxSumRow = rowNum;
+            }
+
+            rowNum++;
+        }
+
+        return maxSumRow;
+    }
+}
