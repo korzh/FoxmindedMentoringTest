5b268c8349623c8f5f09d67227c111c7c6d1fccf
EasyQuery team lead <easyquery@korzh.com>
2022-10-30 01:12:28 +0300

    Console app

---
+++ src/MaxSumRowApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using MaxSumRowApp;
+
+string? filePath;
+
+if (args.Length >0) {
+    filePath = args[0];
+}
+else {
+    Console.WriteLine("Enter the path to the input file:");
+    filePath = Console.ReadLine();
+    if (filePath == null) {
+        Console.WriteLine("No file name defined! Please try again");
+        return;
+    }
+}
+
+Console.Write($"Processing {filePath}...");
+
+var processor = new TextProcessor(new StreamReader(filePath));
+var rowNum = await processor.GetMaxSumRowNum();
+Console.WriteLine("done!");
+Console.WriteLine();
+
+Console.WriteLine($"Max SUM row: {rowNum}");
+
+if (processor.DefectiveRows.Count > 0) {
+    Console.WriteLine("Defective rows: " + string.Join(", ", processor.DefectiveRows));
+}
